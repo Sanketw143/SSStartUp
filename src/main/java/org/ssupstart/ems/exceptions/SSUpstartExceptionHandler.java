c1874cd3c46a703aeca5cb606c15d0418f03507b
HarshWadkute <hvw.769@gmail.com>
2025-11-17 14:54:12 +0530

    Docker setup commit V3

--- src/main/java/org/ssupstart/ems/exceptions/SSUpstartExceptionHandler.java
+++ src/main/java/org/ssupstart/ems/exceptions/SSUpstartExceptionHandler.java
@@ -3,15 +3,14 @@ package org.ssupstart.ems.exceptions;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.ssupstart.ems.constants.ExceptionsConstants;
 import org.ssupstart.ems.responses.APIResponse;
 
 @RestControllerAdvice("org.ssupstart.ems")
-public class GlobalExceptionHandler {
+public class SSUpstartExceptionHandler {
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         if (ex.getRootCause() != null && ex.getRootCause().getMessage().contains("UNIQUE") || ex.getRootCause().getMessage().contains("duplicate")) {
             return ResponseEntity.status(HttpStatus.CONFLICT)
@@ -23,19 +22,19 @@ public class GlobalExceptionHandler {
 
 
     //ToDo : check this flow properly
-    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<APIResponse> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new APIResponse<>(ex.getMessage(), ExceptionsConstants.SOMETHING_WENT_WRONG, false));
     }
 
-    @ExceptionHandler(EmailNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(EmailNotFoundException.class)
     public ResponseEntity<APIResponse> handleEmailNotFound(EmailNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new APIResponse<>(ex.getMessage(), ExceptionsConstants.SOMETHING_WENT_WRONG, false));
     }
 
-    @ExceptionHandler(EmployeeNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<APIResponse> handleEmployeeNotFound(EmployeeNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new APIResponse<>(ex.getMessage(), ExceptionsConstants.SOMETHING_WENT_WRONG, false));
